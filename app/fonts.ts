abc4ff4a1844fdedcdf22ae28ab9b57f44299b2a
Malaydewangan09 <malay.worldref@gmail.com>
2025-03-05 10:48:40 +0530

    feat: update pages

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,5 +1,4 @@
 import { Playfair_Display, Inter, Montserrat } from 'next/font/google'
-import url('https://fonts.googleapis.com/css2?family=GFS+Didot&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
 // Define the fonts with the variants you need
 export const playfair = Playfair_Display({ 
   subsets: ['latin'],
